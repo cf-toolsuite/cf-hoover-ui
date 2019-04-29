78174ef857064256c4c363d6a8cc51279a64cb54
Chris Phillipson <cphillipson@pivotal.io>
2019-04-29 08:42:38 -0700

    Strip away Vaadin * this branch exists solely to test reactive-feign at startup

--- src/main/java/io/pivotal/CfHooverUiApplication.java
+++ src/main/java/io/pivotal/CfHooverUiApplication.java
@@ -1,19 +1,14 @@
 package io.pivotal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.reactive.ClientHttpConnector;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
-import io.netty.channel.ChannelOption;
 import reactivefeign.spring.config.EnableReactiveFeignClients;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.tcp.TcpClient;
 
 @SpringBootApplication
 @EnableReactiveFeignClients
@@ -23,39 +18,17 @@ public class CfHooverUiApplication {
         SpringApplication.run(CfHooverUiApplication.class, args);
     }
 
-    @Configuration
-    static class ApplicationSecurityOverride extends WebSecurityConfigurerAdapter {
-
-        @Override
-        public void configure(HttpSecurity web) throws Exception {
-            web.csrf().disable();
-            web.authorizeRequests().antMatchers("/**").permitAll();
-        }
-
-        @Override
-        public void configure(WebSecurity web) throws Exception {
-            web.ignoring().antMatchers(
-                    // Vaadin Flow static resources
-                    "/VAADIN/**",
-
-                    // the standard favicon URI
-                    "/favicon.ico",
-
-                    // the robots exclusion standard
-                    "/robots.txt",
-
-                    // web application manifest
-                    "/manifest.webmanifest", "/sw.js", "/offline-page.html",
-
-                    // (development mode) static resources
-                    "/frontend/**",
-
-                    // (development mode) webjarsØ
-                    "/webjars/**",
-
-                    // (production mode) static resources
-                    "/frontend-es5/**", "/frontend-es6/**");
-        }
+    @Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		return http
+				.authorizeExchange()
+					.matchers(
+						PathRequest.toStaticResources().atCommonLocations(),
+						EndpointRequest.toAnyEndpoint())
+						.permitAll()
+					.pathMatchers("/accounting/**","/snapshot/**")
+						.permitAll()
+					.and().build();
     }
-
+    
 }
