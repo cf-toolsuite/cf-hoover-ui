01b9b1b3d797ee98de6d518c17574d72744520d8
Chris Phillipson <cphillipson@pivotal.io>
2019-06-03 12:38:35 -0700

    Sort velocity metrics by ascending trimeframe

--- src/main/java/io/pivotal/cfapp/domain/VelocityCount.java
+++ src/main/java/io/pivotal/cfapp/domain/VelocityCount.java
@@ -1,6 +1,5 @@
 package io.pivotal.cfapp.domain;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -16,9 +15,9 @@ public class VelocityCount {
     private Long count;
 
     public static List<VelocityCount> listOf(Map<String, Long> metrics) {
-        return metrics.entrySet().stream()
+        Map<String, Long> ordinalMetrics = Timeframe.sort(metrics);
+        return ordinalMetrics.entrySet().stream()
             .map(e -> new VelocityCount(e.getKey(), e.getValue()))
-            .sorted(Comparator.comparing(VelocityCount::getTimeframe))
             .collect(Collectors.toList());
     }
 }
