23c74cab50e669468ed346b23235fe98f249e833
Chris Phillipson <cphillipson@pivotal.io>
2019-12-03 11:48:17 -0800

    Add @ConfigurationPropertiesScan to fix startup issue

--- src/main/java/io/pivotal/cfapp/CfHooverUiApplication.java
+++ src/main/java/io/pivotal/cfapp/CfHooverUiApplication.java
@@ -2,10 +2,12 @@ package io.pivotal.cfapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CfHooverUiApplication {
 
     public static void main(String[] args) {
